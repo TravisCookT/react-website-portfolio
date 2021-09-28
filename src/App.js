8b3286647cf5e7757bfa0a0eccbcca89e986c373
Travis Cook <trey.t.cook@gmail.com>
2021-09-27 21:25:26 -0500

    Changed web-design to a single page style
    Switched to styled components and smooth scroll.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/pages/Home';
+import {BrowserRouter as Router} from 'react-router-dom'
+
 
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component = {Home} />
-        </Switch>
-      </Router>
-
-    </>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {FaBars} from 'react-icons/fa'
+import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtnLinks} from './NavbarElements'
+
+const Navbar= () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to="/">Travis</NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to='about'>ABOUT</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='work'>WORK</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='contact'>CONTACT</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    
+                </NavbarContainer>
+            </Nav>
+        </>
+    );
+}
+
+export default Navbar
