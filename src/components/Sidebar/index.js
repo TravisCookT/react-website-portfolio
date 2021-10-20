4a3886b4f8ae4336c174baa725fe5393a1a7db4f
Travis Cook <trey.t.cook@gmail.com>
2021-10-19 20:06:40 -0500

    Added initial sidebar functionality

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarLink, SidebarMenu} from './SidebarElements'
+
+const Sidebar = ({isOpen, toggle}) => {
+    return (
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+            <Icon onClick={toggle}>
+                <CloseIcon />
+            </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about" onClick={toggle}>
+                        About
+                    </SidebarLink>
+                    <SidebarLink to="work" onClick={toggle}>
+                        Work
+                    </SidebarLink>
+                    <SidebarLink to="contact" onClick={toggle}>
+                        Contact
+                    </SidebarLink>
+                </SidebarMenu>
+            </SidebarWrapper>
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar
